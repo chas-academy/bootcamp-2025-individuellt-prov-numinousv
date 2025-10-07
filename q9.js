86c6a5ff7c0244a793fb97e767e8c85f34bdf86a
@numinousv <@wvrdian333@proton.me>
2025-10-07 12:49:57 +0200

    Klart

--- q9.js
+++ q9.js
@@ -1,6 +1,7 @@
 // skapa funktionerna som anropas i myCalculator så att de funkar så som koden och alla "cases" antyder
 
 export function myCalculator(operator, num1, num2) {
+
   switch (operator) {
     case "+":
       return myAddFunction(num1, num2);
@@ -17,4 +18,19 @@ export function myCalculator(operator, num1, num2) {
     default:
       return "Error operator not found!";
   }
+}
+function myAddFunction (num1, num2) {
+  return num1 + num2;
+}
+function mySubtractFunction (num1, num2) {
+  return num1 - num2;
+}
+function myMultiplyFunction (num1, num2) {
+  return num1 * num2;
+}
+function myDivisionFunction (num1, num2) {
+  if (num == 0) {
+    return "error";
+  }
+  return num1/num2;
 }
